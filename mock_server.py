c44555a3d638799c7536fda65b94d30f5c17ec6d
wanghr <wanghr@izkml.com>
2020-12-02 09:15:08 +0800

    修改读取单一json文件，从目录下读取所有.json文件

--- mock_server.py
+++ mock_server.py
@@ -8,53 +8,52 @@
 from flask import abort, jsonify, Flask, request, Response
 import json
 import ast
-# import db
+import os
 import log
-# opt = db.OperationDbInterface()  # 实例化数据库
 log = log.TestLog(logger='main').getlog()  # 实例化log
 
 app = Flask(__name__)
 # 增加配置，支持中文显示
 app.config['JSON_AS_ASCII'] = False
 
+data_config = []
+
 
 def get_data_from_json(*params):
-    for data in params[0]['data']:
-        if params[1] in data['name_interface'] and data['status'] == 1:  # 接口地址匹配
-            for req in data['request_response']:
-                if req['status'] == 1 and \
-                        (ast.literal_eval(params[2]) == req['value_request'] or str(params[2]) == str(req['value_request'])):  # 请求参数匹配
-                    return req['value_response']
+    """
+    处理读取的json文件中是否有匹配的请求数据
+    :param params: 配置文件内容+url+请求参数
+    :return: 响应数据
+    """
+    for data_json in params[0]:
+        for data in data_json['data']:
+            if params[1] in data['name_interface'] and data['status'] == 1:  # 接口地址匹配
+                for req in data['request_response']:
+                    if req['status'] == 1 and \
+                            (ast.literal_eval(params[2]) == req['value_request'] or str(params[2]) == str(req['value_request'])):  # 请求参数匹配
+                        return req['value_response']
 
 
 @app.route('/<path:requirt>', methods=['GET', 'POST'])
 def get_response_by_request(requirt):
-    log.info("request params：%s" % requirt)
+    log.info("[INFO]Request Url：%s" % requirt)
     if request.method == 'GET':
         param_request = request.full_path.split('?')[1]
     elif request.method == 'POST':
-        # print(request.get_data(as_text=True))
         param_request = request.get_data(as_text=True).replace('"', "'")
     else:
         param_request = ''
-    log.info("preprocess params：%s" % param_request)
+    log.info("[INFO]Request Param：%s" % param_request)
     # 从json文件中读取数据
-    with open("E:\\Project\\mock_interface\\config\\pay_center.json", 'r', encoding='utf8') as fp:
-        json_data = json.load(fp)
-    result = get_data_from_json(json_data, str(requirt), str(param_request))
+    for filename in os.listdir("./config"):
+        with open("./config//" + filename, 'r', encoding='utf-8') as fp:
+            data_config.append(json.load(fp))
+    result = get_data_from_json(data_config, str(requirt), str(param_request))
+    log.info("[INFO]Response：%s" % result)
     if result:
         return jsonify(result)
     else:
         return jsonify({'code': 0, 'message': 'json文件中没有匹配的接口地址或请求参数'})
-    # result = opt.select_one('SELECT param_response FROM relation WHERE url_interface="%s" AND param_request="%s" '
-    #                         'AND status=1' % (str(requirt), str(param_request)))
-    # log.info("db result：%s" % result)
-    # if result['data'] and result['code'] == '0000':  # 从db中拿到数据
-    #     return jsonify(ast.literal_eval(result['data']['param_response']))
-    # elif result['code'] != '0000':  # db查询数据失败
-    #     return jsonify({'code': 9, 'message': 'unexpect error on get db data'})
-    # else:  # db中无数据
-    #     return jsonify({'code': 0, 'message': 'db data is null'})
 
 
 if __name__ == '__main__':
--- readme.txt
+++ readme.txt
@@ -1,3 +1,2 @@
 1.mock_server.py需要一直启动，否则api无法调用
-2.db.psc是mysql数据的备份数据库；
-3.注意db连接的正确性。
+2.config目录下的json文件需要注意格式
